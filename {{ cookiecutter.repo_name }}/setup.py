fb6fb279aae392e0cb038f42f2b24aff4ee1be48
tomdgr <tomdgrande@gmail.com>
2023-02-21 19:58:19 +0100

    change setup

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -1,10 +1,46 @@
+import os
 from setuptools import find_packages, setup
 
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(file_name):
+    return open(os.path.join(os.path.dirname(__file__), file_name)).read()
+
+
+
+install_requires = [
+    'numpy',
+    'pandas',
+    'pytest'
+]
+
+setup_requirements = [
+    'pytest-runner',
+    'better-setuptools-git-version'
+]
+
+test_requirements = [
+    'pytest',
+    'nbformat'
+]
+
 setup(
-    name='src',
-    packages=find_packages(),
-    version='0.1.0',
-    description='{{ cookiecutter.project_description }}',
-    author='{{ cookiecutter.author_name }}',
-    license='{% if cookiecutter.open_source_license == 'MIT' %}MIT{% elif cookiecutter.open_source_license == 'BSD-3-Clause' %}BSD-3{% endif %}',
+    author='{{cookiecutter.author_name}}',
+    name="{{cookiecutter.project_name}}",
+    # version="0.0.1",
+    version_config={
+      "version_format": "{tag}.dev{sha}",
+      "starting_version": "0.0.1"
+    },
+    description="{{cookiecutter.project_description}}",
+    long_description=open('README.md').read(),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    install_requires=install_requires
 )
